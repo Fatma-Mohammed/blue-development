0c125cb3ff0136579c784a09e5a1170fb70a2692
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-03 15:43:20 +0200

    Add product cruds & admin auth

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,23 +4,77 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use Illuminate\Support\Facades\Gate;
 class ProductController extends Controller
 {
+    public function index()
+    {
+        $products = Product::all();
+        return view('home', [
+            'products' => $products,
+        ]);
+    }
     public function create(){
 
-      
-        return view('products/create');
+        if (Gate::allows('isAdmin')) {
+
+            return view('products/create');
+    
+        } else {
+    
+            dd('You are not Admin');
+    
+        }
+    
+        
    
    }
 
    public function store(Request $request)
    {
 
-        // $attributes['avatar'] = request('avatar')->store('avatars');
-       Product::create([
-           'name' => $request->name,
-           'avatar' => $request->avatar->store('avatars'),
+    if (Gate::allows('isAdmin')) {
+
+        Product::create([
+            'name' => $request->name,
+            'avatar' => $request->avatar->store('avatars'),
+        ]);
+        return redirect()->route("home");
+
+    } else {
+
+        dd('You are not Admin');
+
+    }
+        
+
+   }
+
+   public function destroy(Request $request)
+   {
+       $productId = $request->product;
+       $product = product::find($productId);
+       $product->delete();
+
+       return redirect()->route("posts.index");
+   }
+
+   public function edit(Request $request)
+   {
+
+       $productId = $request->product;
+       $product = Product::find($productId);
+       return view('products/edit', [
+           'product' => $product,
+
        ]);
-       return redirect()->route("home");
+   }
+   public function update(Request $request)
+   {   
+            
+       $product = $request->only(["name","avatar"]); 
+       Product::find($request->product)->update($product);
+           
+       return redirect()->route('home');
    }
 }
