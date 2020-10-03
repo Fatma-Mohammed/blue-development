57b01cb5b73349affb8461181ca3a6e8155bb925
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-03 15:54:08 +0200

    Add product CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/', function () {
 
 
 Auth::routes(['verify' => true]);
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'ProductController@index')->name('home');
 
 Route::get('/products/create','ProductController@create')->name('product.create');
-
 Route::post('/products','ProductController@store')->name('product.store');
+Route::get('/products/{product}/edit','ProductController@edit') -> name('product.edit');
+Route::put('/products/{product}','ProductController@update') -> name('product.update');
+Route::delete('/products/{product}', 'ProductController@destroy') -> name('product.destroy');
+Route::get('/products/{product}','ProductController@show')->name('product.show');
