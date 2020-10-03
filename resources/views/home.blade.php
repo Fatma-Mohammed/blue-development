06d4f633c13ea23980c488da38d7d1184927fb36
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-03 15:53:28 +0200

    List products in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,45 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+  <div class="row justify-content-center">
+    
+      
+        
+        <div class="container m-5">
+          <a href="{{route('product.create')}}" class="btn btn-success mb-5">Create Product</a>
+          <table class="table table-sm">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">name</th>
+                <th colspan="3">Action</th>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($products as $product)
+              <tr>
+                <th scope="row">{{ $product->id }}</th>
+                <td>{{ $product->name }}</td>
+                <td><a href="{{route('product.show',['product'=>$product->id])}}" class="btn btn-secondary btn-sm">view</a>
+                <td><a href="{{route('product.edit',['product'=>$product->id])}}" class="btn btn-secondary btn-sm">Edit</a></td>
+                <td>
+                  <form method="POST" action="{{route('product.destroy',['product' => $product->id])}}">
+                    @csrf @method('delete')
+                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure that you want to delete this product ?')">
+                      Delete </button>
+                  </form>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
         </div>
-    </div>
+
+
+      
+    
+  </div>
 </div>
 @endsection
