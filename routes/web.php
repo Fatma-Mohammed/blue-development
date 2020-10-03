0f41d65abdf752a9feab7c508966db97e798979e
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-03 07:16:28 +0200

    Revert "Create basic product"
    This reverts commit f35dc65650ebc524990c3455a946fe3711fd717d.

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,3 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/products/create','ProductController@create')->name('product.create');
-
-Route::post('/products','ProductController@store')->name('product.store');
