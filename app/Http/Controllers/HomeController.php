4cfc273bfd75526534cca9f441d4ac005ae6fe92
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-04 02:40:20 +0200

    Render products from home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Product;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::all();
+        return view('home',[
+            'products' => $products,
+
+        ]);
     }
 }
