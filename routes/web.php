9821679dc8933e2e3734e95c927589aabcf7ad98
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-10-05 09:16:58 +0200

    Add admin middkeware on routes

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,21 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['verify' => true]);
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/products','ProductController@index')->name('products');
-Route::get('/products/create','ProductController@create')->name('product.create');
-Route::post('/products','ProductController@store')->name('product.store');
-Route::get('/products/{product}/edit','ProductController@edit') -> name('product.edit');
-Route::put('/products/{product}','ProductController@update') -> name('product.update');
-Route::delete('/products/{product}', 'ProductController@destroy') -> name('product.destroy');
-Route::get('/products/{product}','ProductController@show')->name('product.show');
+
+    Route::group(['middleware' => 'admin'], function () {
+    Route::get('/products', 'ProductController@index')->name('products');
+    Route::get('/products/create', 'ProductController@create')->name('product.create');
+    Route::post('/products', 'ProductController@store')->name('product.store');
+    Route::get('/products/{product}/edit', 'ProductController@edit')->name('product.edit');
+    Route::put('/products/{product}', 'ProductController@update')->name('product.update');
+    Route::delete('/products/{product}', 'ProductController@destroy')->name('product.destroy');
+});
+
+Route::get('/products/{product}', 'ProductController@show')->name('product.show');
 
 
 Route::post('cart/{product_id}', 'CartController@addProductToCart')->name('cart.add');
-Route::get('/cart','CartController@show')->name('cart.show');
-Route::delete('/cart/{product}', 'CartController@removeProductFromCart') -> name('cart.product.destroy');
+Route::get('/cart', 'CartController@show')->name('cart.show');
+Route::delete('/cart/{product}', 'CartController@removeProductFromCart')->name('cart.product.destroy');
 
-Route::get('/search','HomeController@index')->name('search');
-
+Route::get('/search', 'HomeController@index')->name('search');
